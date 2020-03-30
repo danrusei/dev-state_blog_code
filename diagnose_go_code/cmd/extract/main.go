fdf99260c1d49412d5b785ecf82945be24f63602
Dan Rusei <dan.rusei@gmail.com>
2020-03-30 17:42:52 +0300

    extarct from csv to txt

--- diagnose_go_code/cmd/extract/main.go
+++ diagnose_go_code/cmd/extract/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+
+	csvfile, err := os.Open("../../csv_files/test.csv")
+	if err != nil {
+		log.Fatalln("Couldn't open the csv file", err)
+	}
+	defer csvfile.Close()
+
+	r := csv.NewReader(csvfile)
+
+	txtfile, err := os.OpenFile("../../csv_files/test.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer txtfile.Close()
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		txtfile.WriteString(record[3] + "#" + record[4] + "\n")
+	}
+}
