4282e8baee706d1da548e3bedc83bd5e045b43e7
Dan Rusei <dan.rusei@gmail.com>
2024-02-10 21:17:08 +0200

    another car selection for functional options pattern

--- builder_pattern/builder_Go/main.go
+++ builder_pattern/builder_Go/main.go
@@ -10,19 +10,21 @@ import (
 func main() {
 	// Example usage of Builder Pattern:
 	builder := car1.NewCarBuilder(car1.EquipedGold).
-		SetColor("Silver").
+		SetColor("Brown").
 		SetPetrolEngine(car1.Petrol225HP)
 
 	car_ex1 := builder.Build()
+	fmt.Println("===========================================")
 	fmt.Printf("Builder Pattern: %v\n", car_ex1)
+	fmt.Println("===========================================")
 
 	// ---------------------------------------------
 
 	// Example usage of Functional Options Pattern
 	car_ex2 := car2.NewCar(
-		car2.WithEquipment(car2.EquipedGold),
-		car2.WithColor("Silver"),
-		car2.WithPetrolEngine(car2.Petrol225HP),
+		car2.WithEquipment(car2.EquipedSilver),
+		car2.WithColor("White"),
+		car2.WithElectricEngine(car2.Electric420kW),
 	)
 
 	fmt.Printf("Functional Options Pattern: %s\n", car_ex2)
